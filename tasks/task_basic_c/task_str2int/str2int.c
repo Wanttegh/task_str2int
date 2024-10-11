1652f1ef647606233601907962c0ccef51366a13
howard <wantte@yandex.ru>
2024-10-12 00:35:06 +0300

    Последний коммит, точно работает

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,9 +1,6 @@
 #include <assert.h>
 #include "str2int.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <stdbool.h>
 // если строка пустая или состоит из одного символа, который не цифра
 
 int str_size(const char * str){
@@ -22,45 +19,26 @@ int access(const char * str){
 	return 1;
 }
 
-// проверка, если число превосходит тип int
-
-int more_than_int(const char * str, int sign){
-	const char * strc = str;
-	if (str_size(strc) < 10) return 0;
-	else if (str_size(strc) == 10){
-		if (sign == -1){
-			char * int_min = "2147483648";
-			while (*int_min >= *strc) {strc++; int_min++;}
-			if (*int_min == '8') return 0;
-			else return 1;
-		}
-		else {
-			char * int_max = "2147483647";
-			while (*int_max >= *strc) {strc++; int_max++;}
-			if (*int_max == '7') return 0;
-			else return 1;
-		}
-	}
-	else return 1;
-}
-
 int str2int(const char *str) {
-    assert(str[0] != '\0');
-    assert(access(str) != 0);
+    assert(*str != '\0');
     int sign = 1;
-    int num = 0;  
+    int num = 0;
+    const char * strc = str;  
     if (str[0] == '-' || str[0] == '+'){
     	sign = (*str == '-') ? -1 : 1;
 	str++;
     } 
     while (*str == ' ') str++;
-
-    assert(more_than_int(str, sign) != 1);
+    assert(access(str) != 0);
+    char * ptr;
 
     while (*str){
-    	if (*str >= '0' && *str <= '9') num = num*10 + (*str - '0');
-	else assert(*str >= '0' && *str <= '9');
-	str++;
+    	    if (*str >= '0' && *str <= '9') num = num*10 + (*str - '0');
+	        else assert(*str >= '0' && *str <= '9');
+        	str++;
     }
+
+    assert(num*sign == strtol(strc, &ptr, 10));
+
     return num * sign;
 }
